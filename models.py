7941186f9f6eaf3b5c82473ffee073f48504954e
HappyToast <yrs1107@naver.com>
2023-11-21 14:37:36 +0900

    models with goal embedded

--- models.py
+++ models.py
@@ -114,6 +114,7 @@ class Discriminator(torch.nn.Module):
                 self.all_inst = torch.arange(n_inst, 
                     dtype=seq_end_frame.dtype, device=seq_end_frame.device)
             s, _ = self.rnn(s)
+            #s = self.rnn(s)
             s = s[(self.all_inst[:n_inst], torch.clip(seq_end_frame, max=s.size(1)-1))]
         return self.mlp(s)
 
@@ -129,7 +130,7 @@ class ACModel(torch.nn.Module):
                 torch.nn.ReLU6(),
                 torch.nn.Linear(1024, 512),
                 torch.nn.ReLU6(),
-                torch.nn.Linear(512, value_dim)
+                torch.nn.Linear(512, value_dim)     # value_dim = critic 개수
             )
             i = 0
             for n, p in self.mlp.named_parameters():
@@ -197,8 +198,9 @@ class ACModel(torch.nn.Module):
                 if n_inst > self.all_inst.size(0):
                     self.all_inst = torch.arange(n_inst, 
                         dtype=seq_end_frame.dtype, device=seq_end_frame.device)
-                s, _ = self.rnn(s)
-                s = s[(self.all_inst[:n_inst], torch.clip(seq_end_frame, max=s.size(1)-1))]
+                s, _ = self.rnn(s)          # output s [n, 4, 256]
+                s = s[(self.all_inst[:n_inst], torch.clip(seq_end_frame, max=s.size(1)-1))] # output s [n', 256]
+
             if g is not None:
                 s = torch.cat((s, g), -1)
             latent = self.mlp(s)
@@ -233,6 +235,7 @@ class ACModel(torch.nn.Module):
         s = s.view(*s.shape[:-1], -1, self.state_dim)
         return self.ob_normalizer(s) if norm else s, g
 
+    # get value
     def eval_(self, s, seq_end_frame, g, unnorm):
         v = self.critic(s, seq_end_frame, g)
         if unnorm and self.value_normalizer is not None:
@@ -242,26 +245,240 @@ class ACModel(torch.nn.Module):
     def act(self, obs, seq_end_frame, stochastic=None, unnorm=False):
         if stochastic is None:
             stochastic = self.training
-        s, g = self.observe(obs)
-        pi = self.actor(s, seq_end_frame, g)
+        s, g = self.observe(obs)            # normalize state
+        pi = self.actor(s, seq_end_frame, g)    # forward of Actor, outputs normal distribution
         if stochastic:
             a = pi.sample()
             lp = pi.log_prob(a)
             if g is not None:
                 g = g[...,:self.goal_dim]
-            return a, self.eval_(s, seq_end_frame, g, unnorm), lp
+            return a, self.eval_(s, seq_end_frame, g, unnorm), lp   # actions, values, log_probs
         else:
-            return pi.mean
+            return pi.mean,     # [num_envs, action_dim]
 
     def evaluate(self, obs, seq_end_frame, unnorm=False):
-        s, g = self.observe(obs)
+        s, g = self.observe(obs)                        # normalize state
         if g is not None:
             g = g[...,:self.goal_dim]
-        return self.eval_(s, seq_end_frame, g, unnorm)
+        return self.eval_(s, seq_end_frame, g, unnorm)  # get value
     
     def forward(self, obs, seq_end_frame, unnorm=False):
-        s, g = self.observe(obs)
+        s, g = self.observe(obs)                    # normalize state
         pi = self.actor(s, seq_end_frame, g)
         if g is not None:
             g = g[...,:self.goal_dim]
         return pi, self.eval_(s, seq_end_frame, g, unnorm)
+
+
+class ACModel_gembed(torch.nn.Module):
+
+    class Critic(torch.nn.Module):
+        def __init__(self, state_dim, goal_dim, upper_g_dim, lower_g_dim, value_dim=1, latent_dim=256, glatent_dim = 16):
+            super().__init__()
+            self.rnn = torch.nn.GRU(state_dim + upper_g_dim, latent_dim + glatent_dim, batch_first=True)
+            #self.rnn_g = torch.nn.GRU(upper_g_dim, glatent_dim, batch_first=True)
+
+            self.mlp = torch.nn.Sequential(
+                torch.nn.Linear(latent_dim+glatent_dim+upper_g_dim+lower_g_dim, 1024),
+                torch.nn.ReLU6(),
+                torch.nn.Linear(1024, 512),
+                torch.nn.ReLU6(),
+                torch.nn.Linear(512, value_dim)     # value_dim = critic 개수
+            )
+            i = 0
+            for n, p in self.mlp.named_parameters():
+                if "bias" in n:
+                    torch.nn.init.constant_(p, 0.)
+                elif "weight" in n:
+                    torch.nn.init.uniform_(p, -0.0001, 0.0001)
+                    i += 1
+            self.all_inst = torch.arange(0)
+
+        def forward(self, s, seq_end_frame, embed_g = None, upper_g=None, lower_g=None):
+            if self.rnn is None:
+                s = s.view(s.size(0), -1)
+            else:
+                n_inst = s.size(0)
+                if n_inst > self.all_inst.size(0):
+                    self.all_inst = torch.arange(n_inst, 
+                        dtype=seq_end_frame.dtype, device=seq_end_frame.device)
+                if embed_g is not None:
+                    s = torch.cat((s, embed_g), -1)
+                s, _ = self.rnn(s)
+                s = s[(self.all_inst[:n_inst], torch.clip(seq_end_frame, max=s.size(1)-1))]
+            
+            # if embed_g is not None:
+            #     if self.rnn_g is None:
+            #         embed_g = embed_g.view(embed_g.size(0), -1)
+            #     else:
+            #         n_inst = embed_g.size(0)
+            #         if n_inst > self.all_inst.size(0):
+            #             self.all_inst = torch.arange(n_inst, dtype=seq_end_frame.dtype, device=seq_end_frame.device)
+            #     embed_g, _ = self.rnn_g(embed_g)          
+            #     embed_g = embed_g[(self.all_inst[:n_inst], torch.clip(seq_end_frame, max=embed_g.size(1)-1))] # output g [n_envs, 64]
+
+            # if embed_g is not None:
+            #     s = torch.cat((s, embed_g), -1)     # 256 + 16
+            if upper_g is not None:
+                s = torch.cat((s, upper_g), -1)     # 256 + 16 + 18
+            if lower_g is not None:
+                s = torch.cat((s, lower_g), -1)     # 256 + 16 + 18 + 8
+
+            return self.mlp(s)
+
+    class Actor(torch.nn.Module):
+        def __init__(self, state_dim, act_dim, goal_dim, upper_g_dim, lower_g_dim, latent_dim=256, glatent_dim = 16, init_mu=None, init_sigma=None):
+            super().__init__()
+            self.rnn = torch.nn.GRU(state_dim + upper_g_dim, latent_dim + glatent_dim, batch_first=True)
+            #self.rnn_g = torch.nn.GRU(upper_g_dim, glatent_dim, batch_first=True)
+
+            self.mlp = torch.nn.Sequential(
+                torch.nn.Linear(latent_dim+glatent_dim+upper_g_dim+lower_g_dim, 1024),
+                torch.nn.ReLU6(),
+                torch.nn.Linear(1024, 512),
+                torch.nn.ReLU6()
+            )
+            self.mu = torch.nn.Linear(512, act_dim)
+            self.log_sigma = torch.nn.Linear(512, act_dim)
+            with torch.no_grad():
+                if init_mu is not None:
+                    if torch.is_tensor(init_mu):
+                        mu = torch.ones_like(self.mu.bias)*init_mu
+                    else:
+                        mu = np.ones(self.mu.bias.shape, dtype=np.float32)*init_mu
+                        mu = torch.from_numpy(mu)
+                    self.mu.bias.data.copy_(mu)
+                    torch.nn.init.uniform_(self.mu.weight, -0.00001, 0.00001)
+                if init_sigma is None:
+                    torch.nn.init.constant_(self.log_sigma.bias, -3)
+                    torch.nn.init.uniform_(self.log_sigma.weight, -0.0001, 0.0001)
+                else:
+                    if torch.is_tensor(init_sigma):
+                        log_sigma = (torch.ones_like(self.log_sigma.bias)*init_sigma).log_()
+                    else:
+                        log_sigma = np.log(np.ones(self.log_sigma.bias.shape, dtype=np.float32)*init_sigma)
+                        log_sigma = torch.from_numpy(log_sigma)
+                    self.log_sigma.bias.data.copy_(log_sigma)
+                    torch.nn.init.uniform_(self.log_sigma.weight, -0.00001, 0.00001)
+                self.all_inst = torch.arange(0)
+
+        def forward(self, s, seq_end_frame, embed_g=None, upper_g=None, lower_g=None):
+            if self.rnn is None:
+                s = s.view(s.size(0), -1)
+            else:
+                n_inst = s.size(0)
+                if n_inst > self.all_inst.size(0):
+                    self.all_inst = torch.arange(n_inst, 
+                        dtype=seq_end_frame.dtype, device=seq_end_frame.device)
+                if embed_g is not None:
+                    s = torch.cat((s, embed_g), -1)
+                s, _ = self.rnn(s)
+                s = s[(self.all_inst[:n_inst], torch.clip(seq_end_frame, max=s.size(1)-1))]
+            
+            # if embed_g is not None:
+            #     if self.rnn_g is None:
+            #         embed_g = embed_g.view(embed_g.size(0), -1)
+            #     else:
+            #         n_inst = embed_g.size(0)
+            #         if n_inst > self.all_inst.size(0):
+            #             self.all_inst = torch.arange(n_inst, dtype=seq_end_frame.dtype, device=seq_end_frame.device)
+            #     embed_g, _ = self.rnn_g(embed_g)          
+            #     embed_g = embed_g[(self.all_inst[:n_inst], torch.clip(seq_end_frame, max=embed_g.size(1)-1))] # output g [n_envs, 16]
+            # if embed_g is not None:
+            #     s = torch.cat((s, embed_g), -1)     # 256 + 16
+            if upper_g is not None:
+                s = torch.cat((s, upper_g), -1)     # 256 + 16 + 18
+            if lower_g is not None:
+                s = torch.cat((s, lower_g), -1)     # 256 + 16 + 18 + 8
+
+            latent = self.mlp(s)
+            mu = self.mu(latent)
+            sigma = torch.exp(self.log_sigma(latent)) + 1e-8
+            return torch.distributions.Normal(mu, sigma)
+
+    def __init__(self, state_dim: int, act_dim: int, goal_dim: int=0, upper_goal_dim: int=0, lower_goal_dim: int=0, value_dim: int=1, 
+        normalize_value: bool=True,
+        init_mu:Optional[torch.Tensor or float]=None, init_sigma:Optional[torch.Tensor or float]=None
+    ):
+        super().__init__()
+        self.state_dim = state_dim
+        self.goal_dim = goal_dim
+        self.upper_goal_dim = upper_goal_dim
+        self.lower_goal_dim = lower_goal_dim
+        self.actor = self.Actor(state_dim, act_dim, self.goal_dim, self.upper_goal_dim, self.lower_goal_dim, init_mu=init_mu, init_sigma=init_sigma)
+        self.critic = self.Critic(state_dim, goal_dim, self.upper_goal_dim, self.lower_goal_dim, value_dim)
+        self.ob_normalizer = RunningMeanStd(state_dim, clamp=5.0)
+        self.goal_normalizer = RunningMeanStd(upper_goal_dim, clamp=5.0)
+        if normalize_value:            
+            self.value_normalizer = DiagonalPopArt(value_dim, 
+                self.critic.mlp[-1].weight, self.critic.mlp[-1].bias)
+        else:
+            self.value_normalizer = None
+            
+    def observe(self, obs, norm=True):
+        if self.goal_dim > 0:
+            s = obs[:, :-self.goal_dim]
+            g = obs[:, -self.goal_dim:]
+        else:
+            s = obs
+            g = None
+        s = s.view(*s.shape[:-1], -1, self.state_dim)
+
+        #! embedding!!!!
+        embed_g = g[:, :-(self.upper_goal_dim+self.lower_goal_dim)]
+        upper_g = g[:, -(self.upper_goal_dim+self.lower_goal_dim):-self.lower_goal_dim]
+        lower_g = g[:, -self.lower_goal_dim:]
+
+        embed_g = embed_g.view(embed_g.shape[0], -1, self.upper_goal_dim)       # [n_envs, ob_horizon, upper_goal_dim]
+        return self.ob_normalizer(s) if norm else s, self.goal_normalizer(embed_g) if norm else embed_g, upper_g, lower_g
+    
+
+    # get value
+    def eval_(self, s, seq_end_frame, embed_g, upper_g, lower_g, unnorm):
+        v = self.critic(s, seq_end_frame, embed_g, upper_g, lower_g)
+        if unnorm and self.value_normalizer is not None:
+            v = self.value_normalizer(v, unnorm=True)
+        return v
+
+    def act(self, obs, seq_end_frame, stochastic=None, unnorm=False):
+        if stochastic is None:
+            stochastic = self.training
+        s, embed_g, upper_g, lower_g = self.observe(obs)            # normalize state
+        pi = self.actor(s, seq_end_frame, embed_g, upper_g, lower_g)    # forward of Actor, outputs normal distribution
+        if stochastic:
+            a = pi.sample()
+            lp = pi.log_prob(a)
+            #! embedding!!!!
+            if embed_g is not None:
+                embed_g = embed_g
+            if upper_g is not None:
+                upper_g = upper_g
+            if lower_g is not None:
+                lower_g = lower_g
+                # g = g[...,:self.goal_dim]
+            return a, self.eval_(s, seq_end_frame, embed_g, upper_g, lower_g, unnorm), lp   # actions, values, log_probs
+        else:
+            return pi.mean,     # [num_envs, action_dim]
+
+    def evaluate(self, obs, seq_end_frame, unnorm=False):
+        s, embed_g, upper_g, lower_g = self.observe(obs)                        # normalize state
+        if embed_g is not None:
+            embed_g = embed_g
+        if upper_g is not None:
+            upper_g = upper_g
+        if lower_g is not None:
+            lower_g = lower_g
+
+        return self.eval_(s, seq_end_frame, embed_g, upper_g, lower_g, unnorm)  # get value
+    
+    def forward(self, obs, seq_end_frame, unnorm=False):
+        s, embed_g, upper_g, lower_g = self.observe(obs)                    # normalize state
+
+        pi = self.actor(s, seq_end_frame, embed_g, upper_g, lower_g)
+        if embed_g is not None:
+            embed_g = embed_g
+        if upper_g is not None:
+            upper_g = upper_g
+        if lower_g is not None:
+            lower_g = lower_g
+        return pi, self.eval_(s, seq_end_frame, embed_g, upper_g, lower_g, unnorm)
